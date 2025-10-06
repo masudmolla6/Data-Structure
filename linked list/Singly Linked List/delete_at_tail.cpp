5d2880c28e9c490afb82eac3ac3eb2ab5598eaf8
masudmolla6 <masudmolla2937@gmail.com>
2025-10-06 17:49:47 +0600

    delete at tail

---
+++ linked list/Singly Linked List/delete_at_tail.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int val;
+    Node* next;
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
+    Node* newNode=new Node(val);
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+    }
+    else{
+        tail->next=newNode;
+        tail=newNode;
+    }
+}
+
+void delete_at_tail(Node* &head, Node* &tail){
+    if(head==NULL){
+        return;
+    }
+
+    if(head->next==NULL){
+        head=NULL;
+        tail=NULL;
+        return;
+    }
+
+    Node* temp=head;
+
+    while (temp->next->next !=NULL)
+    {
+        temp=temp->next;
+    }
+    
+    // cout << temp->val << endl;
+
+    Node* deletedNode=temp->next;
+    temp->next=NULL;
+    tail=temp;
+    delete deletedNode;
+
+}
+
+void print_linked_list(Node* head){
+    Node* temp=head;
+    
+    if(head==NULL){
+        return;
+    }
+
+    cout << temp->val << endl;
+
+    print_linked_list(temp->next);
+
+}
+
+int main() {
+    Node* head=NULL;
+    Node* tail=NULL;
+    int val;
+    while (cin >> val && val!=-1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+
+    delete_at_tail(head, tail);
+    
+    print_linked_list(head);
+    
+    return 0;
+}
