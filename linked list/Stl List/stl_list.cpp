2d7c53b18432278895bdfb692bd2c60aeb573910
masudmolla6 <masudmolla2937@gmail.com>
2025-11-09 16:21:43 +0600

    print list using iterator and rang base for loop

--- linked list/Stl List/stl_list.cpp
+++ linked list/Stl List/stl_list.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    list<int>l;
-    cout << l.size()<< endl;
+    list<int>myList(5, 3);
+
+    // print list using iterator 
+
+    // for (auto it = myList.begin(); it!=myList.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
+
+    // print list using Rang base for loop
+    for(auto val:myList){
+        cout << val << endl;
+    }
+    
     return 0;
 }
