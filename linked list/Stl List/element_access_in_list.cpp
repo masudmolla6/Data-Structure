b59d4cdca70046a1661776bffcefedfe025c7cd2
masudmolla6 <masudmolla2937@gmail.com>
2025-11-11 20:03:50 +0600

    access element in list

---
+++ linked list/Stl List/element_access_in_list.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    list<int>myList={98,45,86,45,34,23};
+    // cout << myList.back() << endl;
+    cout << myList.front() << endl;
+    return 0;
+}
