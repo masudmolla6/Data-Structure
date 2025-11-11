7fc10c61dba53dff03ed5adac66cd4c619233a29
masudmolla6 <masudmolla2937@gmail.com>
2025-11-11 20:08:14 +0600

    access element with specific index

--- linked list/Stl List/element_access_in_list.cpp
+++ linked list/Stl List/element_access_in_list.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     list<int>myList={98,45,86,45,34,23};
     // cout << myList.back() << endl;
-    cout << myList.front() << endl;
+    // cout << myList.front() << endl;
+    cout << *next(myList.begin(), 2) << endl;
     return 0;
 }
